7bf8907eced55a617cade4495a0cb3cd429c55f4
Tarp <travistarpley0@gmail.com>
2025-04-02 16:02:38 -0700

    feat: added initializers, changed module name

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module ecommerce-go
+module github.com/fusionxx23/ecommerce-go
 
 go 1.24.1
 
--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/fusionxx23/ecommerce-go/database"
 )
 
+func init() {
+	database.ConnectDatabase()
+	database.SyncDb()
+}
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello from the API!")
