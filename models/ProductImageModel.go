4bd93235fd6daff4702e39ee0ea16f7dd3ae61a4
Tarp <travistarpley0@gmail.com>
2025-04-03 10:38:09 -0700

    feat: added to models

--- models/ProductImageModel.go
+++ models/ProductImageModel.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type ProductImage struct {
 	gorm.Model
-	ID        int64 `gorm:"primaryKey"`
-	Url       string
-	ProductID int64 `gorm:"not null"`
+	ID        int64  `gorm:"primaryKey"`
+	Url       string `gorm:"not null"`
+	ProductID int64  `gorm:"not null"`
 }
--- models/UserModel.go
+++ models/UserModel.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	ID    int64  `gorm:"primaryKey"`
-	Email string `gorm:"unique;not null"`
+	ID                    int64  `gorm:"primaryKey"`
+	Email                 string `gorm:"unique;not null"`
+	DefaultDeliveryInfoId int64
 }
