ae5c6e7008a032d55162c6ea594421842b317e4e
Tarp <travistarpley0@gmail.com>
2025-04-04 18:28:19 -0700

    feat: added jwt utils

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 
 require (
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
--- libs/utils.go
+++ libs/utils.go
@@ -3,6 +3,11 @@ package libs
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"time"
+
+	"github.com/fusionxx23/ecommerce-go/models"
+	"github.com/golang-jwt/jwt"
 )
 
 // generateRandomID generates a random ID of the specified length.
@@ -12,4 +17,31 @@ func GenerateRandomID(length int) (string, error) {
 		return "", err
 	}
 	return hex.EncodeToString(bytes), nil
+}
+
+var (
+	key []byte
+)
+
+func ParseJWT(signedString string, claims jwt.MapClaims) {
+	key = []byte("your-256-bit-secret") // Replace with your actual secret key
+	jwt.ParseWithClaims(signedString, claims, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return key, nil
+	})
+	fmt.Println(claims["sub"])
+
+}
+
+func CreateJWT(user models.User) (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"iss": "auth",
+			"sub": user.Email,                                 // subject of the token
+			"exp": time.Now().Add(86400 * time.Second).Unix(), // 24 hours expiry
+		})
+	s, err := t.SignedString(key)
+	return s, err
 }
