f4fd35df455493d7f1d1bd8f0392b192f8d2521c
Tarp <travistarpley0@gmail.com>
2025-04-05 19:32:10 -0700

    chore: remove unused logic

--- controllers/CheckoutController.go
+++ controllers/CheckoutController.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"fmt"
-	"github.com/fusionxx23/ecommerce-go/libs"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -13,19 +11,4 @@ func CheckoutHandler(s *mux.Router) {
 
 func getCheckout(w http.ResponseWriter, r *http.Request) {
 	// Implement logic to get the cart
-	cartCookie, err := r.Cookie("cart")
-	if err != nil {
-		id, err := libs.GenerateRandomID(32)
-		if err != nil {
-			http.Error(w, "Error generating cart ID", http.StatusInternalServerError)
-			return
-		}
-		// Create a new cart cookie if it doesn't exist
-		cartCookie = &http.Cookie{
-			Name:  "cart",
-			Value: id, // Replace with actual cart initialization logic
-			Path:  "/",
-		}
-	}
-	fmt.Fprintf(w, "Cart: %s", cartCookie.Value)
 }
