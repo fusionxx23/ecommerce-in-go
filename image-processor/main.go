d485583817e83e628b43c9e24ceb39304a973104
Tarp <travistarpley0@gmail.com>
2025-04-12 16:21:19 -0700

    feat: added s3 upload to img processing

--- image-processor/main.go
+++ image-processor/main.go
@@ -3,29 +3,47 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/nfnt/resize"
-	"github.com/streadway/amqp"
-	"gorm.io/gorm"
 	"image"
 	"image/jpeg"
 	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/fusionxx23/ecommerce-go/image-processor/models"
+	"github.com/joho/godotenv"
+	"github.com/nfnt/resize"
+	"github.com/streadway/amqp"
+	"gorm.io/gorm"
 
 	"gorm.io/driver/postgres"
 )
 
 var DB *gorm.DB
+var s3BucketName string
 
 func init() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
 
-	dsn := "host=localhost user=your_user password=your_password dbname=your_db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Access environment variables
+	s3BucketName = os.Getenv("S3_BUCKET_NAME")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Access environment variables
+	databaseURL := os.Getenv("DATABASE_URL")
+	fmt.Println(databaseURL)
+	// Establishing the connection
+
+	DB, err = gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 }
+
 func main() {
-	fmt.Println("Consumer")
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 
 	if err != nil {
@@ -63,29 +81,69 @@ func main() {
 
 			// Determine if the image is portrait or landscape
 			bounds := img.Bounds()
+			orientation := ""
 			if bounds.Dx() > bounds.Dy() {
 				fmt.Println("The image is in landscape orientation.")
+				orientation = "landscape"
 			} else {
+				orientation = "portrait"
 				fmt.Println("The image is in portrait orientation.")
 			}
 
+			if orientation == "landscape" { // no need to optimize landscape picture
+				models.UpdateProductImage(DB, imageID.(string), orientation)
+				continue
+			}
+
 			// Resize the image (example: 100x100)
-			resizedImg400 := resize.Resize(420, 0, img, resize.Lanczos3)
+			resizedImg1260 := resize.Resize(1260, 0, img, resize.Lanczos3)
+			resizedImg420 := resize.Resize(420, 0, img, resize.Lanczos3)
 			resizedImg130 := resize.Resize(130, 0, img, resize.Lanczos3)
 
-			var buf bytes.Buffer
-			err = jpeg.Encode(&buf, resizedImg400, nil)
+			var img1260Buffer bytes.Buffer
+			var img420Buffer bytes.Buffer
+			var img130Buffer bytes.Buffer
+			err = jpeg.Encode(&img420Buffer, resizedImg420, nil)
 			if err != nil {
-				fmt.Printf("Failed to encode resized image: %v\n", err)
+				fmt.Printf("Failed to encode resized image420: %v\n", err)
 				continue
 			}
-			err = jpeg.Encode(&buf, resizedImg130, nil)
+			err = jpeg.Encode(&img130Buffer, resizedImg130, nil)
 			if err != nil {
-				fmt.Printf("Failed to encode resized image: %v\n", err)
+				fmt.Printf("Failed to encode resized image130: %v\n", err)
 				continue
 			}
-			fmt.Printf("Resized image size: %d bytes\n", buf.Len())
+			err = jpeg.Encode(&img1260Buffer, resizedImg1260, nil)
+			if err != nil {
+				fmt.Printf("Failed to encode resized image 1260: %v\n", err)
+				continue
+			}
+			uploadS3Image(img130Buffer, imageID.(string)+"-130.jpg")
+			uploadS3Image(img420Buffer, imageID.(string)+"-420.jpg")
+			uploadS3Image(img1260Buffer, imageID.(string)+"-1260.jpg")
+			models.UpdateProductImage(DB, imageID.(string), orientation)
 		}
 	}()
 	<-forever
+}
+
+func uploadS3Image(b bytes.Buffer, key string) {
+
+	// The session the S3 Uploader will use
+	sess := session.Must(session.NewSession())
+
+	// Create an uploader with the session and default options
+	uploader := s3manager.NewUploader(sess)
+
+	// Upload the file to S3.
+	result, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(s3BucketName),
+		Key:    aws.String(key),
+		Body:   &b,
+	})
+	if err != nil {
+		fmt.Printf("failed to upload file, %v", err)
+		return
+	}
+	fmt.Printf("file uploaded to, %s\n", aws.StringValue(&result.Location))
 }
