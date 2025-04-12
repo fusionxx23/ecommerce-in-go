7cf067dbae870402a4cd61344b2c74e304fb65af
Tarp <travistarpley0@gmail.com>
2025-04-12 16:21:00 -0700

    feat: added rabbitmq init and global connection var

--- http/initializers/rabbitmq.go
+++ http/initializers/rabbitmq.go
@@ -8,11 +8,26 @@ import (
 )
 
 func InitRabbitMQ() {
+	fmt.Println("Go RabbitMQ Tutorial")
+
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	libs.RabbitMqConn = conn
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	libs.RabbitMqConn = conn
 	defer conn.Close()
+
+	fmt.Println("Succesfully connected to RabbitMQ")
+	c, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer c.Close()
+	_, err = c.QueueDeclare("ImageQueue", false, false, false, false, nil)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
 }
