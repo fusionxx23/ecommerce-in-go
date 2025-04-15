e1736a583da946d57459f2998a0115be6f34d0a0
Tarp <travistarpley0@gmail.com>
2025-04-15 12:15:45 -0700

    chore: remove unused code

--- image-processor/main.go
+++ image-processor/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/fusionxx23/ecommerce-go/image-processor/handlers"
 	"github.com/fusionxx23/ecommerce-go/image-processor/libs"
 	"github.com/joho/godotenv"
@@ -79,25 +75,4 @@ func main() {
 		}
 	}()
 	<-forever
-}
-
-func uploadS3Image(b bytes.Buffer, key string) {
-
-	// The session the S3 Uploader will use
-	sess := session.Must(session.NewSession())
-
-	// Create an uploader with the session and default options
-	uploader := s3manager.NewUploader(sess)
-
-	// Upload the file to S3.
-	result, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(s3BucketName),
-		Key:    aws.String(key),
-		Body:   &b,
-	})
-	if err != nil {
-		fmt.Printf("failed to upload file, %v", err)
-		return
-	}
-	fmt.Printf("file uploaded to, %s\n", aws.StringValue(&result.Location))
 }
